27b8882d23354f4c8731a6db62c8458fb4e935ab
Alexey Kuznetsov <ak@axet.ru>
2012-11-22 16:21:01 +0100

    update wget last version

--- src/main/java/com/github/axet/vget/VGet.java
+++ src/main/java/com/github/axet/vget/VGet.java
@@ -2,39 +2,29 @@ package com.github.axet.vget;
 
 import java.io.File;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 import org.apache.commons.lang3.StringUtils;
 
 import com.github.axet.vget.info.VideoInfo;
 import com.github.axet.vget.info.VideoInfo.States;
-import com.github.axet.vget.info.VideoInfo.VideoQuality;
 import com.github.axet.wget.Direct;
 import com.github.axet.wget.DirectRange;
 import com.github.axet.wget.DirectSingle;
+import com.github.axet.wget.RetryWrap;
 import com.github.axet.wget.info.DownloadInfo;
-import com.github.axet.wget.info.URLInfo;
-import com.github.axet.wget.info.ex.DownloadError;
+import com.github.axet.wget.info.ex.DownloadIOError;
+import com.github.axet.wget.info.ex.DownloadInterruptedError;
 import com.github.axet.wget.info.ex.DownloadRetry;
 
 public class VGet {
 
-    ArrayList<Listener> list = new ArrayList<VGet.Listener>();
-    VideoInfo source;
+    VideoInfo info;
     File targetDir;
 
+    File targetForce = null;
+
     File target = null;
-
-    public static interface Listener {
-        public void changed();
-    }
-
-    void changed() {
-        for (Listener l : list) {
-            l.changed();
-        }
-    }
 
     public VGet(URL source, File targetDir) {
         VideoInfo info = new VideoInfo(source);
@@ -47,12 +37,12 @@ public class VGet {
     }
 
     void create(VideoInfo video, File targetDir) {
-        this.source = video;
+        this.info = video;
         this.targetDir = targetDir;
     }
 
     public void setTarget(File file) {
-        target = file;
+        targetForce = file;
     }
 
     /**
@@ -60,24 +50,20 @@ public class VGet {
      * 
      * @return
      */
-    public File getOutput() {
+    public File getTarget() {
         return target;
     }
 
     public VideoInfo getVideo() {
-        return source;
+        return info;
     }
 
     public void download() {
-        try {
-            download(new AtomicBoolean(false), new Runnable() {
-                @Override
-                public void run() {
-                }
-            });
-        } catch (InterruptedException e) {
-            throw new DownloadError(e);
-        }
+        download(new AtomicBoolean(false), new Runnable() {
+            @Override
+            public void run() {
+            }
+        });
     }
 
     /**
@@ -110,82 +96,157 @@ public class VGet {
         return f;
     }
 
-    public void download(final AtomicBoolean stop, final Runnable notify) throws InterruptedException {
-        if (source.empty())
-            source.extract(stop, notify);
+    boolean done(AtomicBoolean stop) {
+        if (stop.get())
+            throw new DownloadInterruptedError("stop");
+        if (Thread.currentThread().isInterrupted())
+            throw new DownloadInterruptedError("interrupted");
 
-        source.setState(States.EXTRACTING_DONE);
+        return false;
+    }
+
+    void retry(AtomicBoolean stop, Runnable notify, Throwable e) {
+        info.setState(States.RETRYING, e);
         notify.run();
 
-        final DownloadInfo info = source.getInfo();
+        boolean retracted = false;
 
-        try {
-            File f;
+        while (!retracted) {
+            for (int i = RetryWrap.RETRY_DELAY; i >= 0; i--) {
+                if (stop.get())
+                    throw new DownloadInterruptedError("stop");
+                if (Thread.currentThread().isInterrupted())
+                    throw new DownloadInterruptedError("interrupted");
 
-            if (target == null) {
-                Integer idupcount = 0;
+                info.setDelay(i);
+                notify.run();
 
-                String sfilename = replaceBadChars(source.getTitle());
-
-                String ct = info.getContentType();
-                if (ct == null)
-                    throw new DownloadRetry("null content type");
-
-                String ext = ct.replaceFirst("video/", "").replaceAll("x-", "");
-
-                do {
-                    String add = idupcount > 0 ? " (".concat(idupcount.toString()).concat(")") : "";
-
-                    f = new File(targetDir, sfilename + add + "." + ext);
-                    idupcount += 1;
-                } while (f.exists());
-                this.target = f;
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException ee) {
+                    throw new DownloadInterruptedError(ee);
+                }
             }
 
-            Direct direct;
-            if (info.range())
-                direct = new DirectRange(info, target);
-            else
-                direct = new DirectSingle(info, target);
+            try {
+                // if we continue to download from old source, and this proxy
+                // server is
+                // down we have to try to extract new info and try to resume
+                // download
 
-            if (info.getContentType() == null || !info.getContentType().contains("video/")) {
-                throw new DownloadRetry("unable to download video, bad content");
-            }
+                DownloadInfo infoOld = info.getInfo();
+                info.extract(stop, notify);
+                DownloadInfo infoNew = info.getInfo();
 
-            source.setState(States.DOWNLOADING);
-            notify.run();
-
-            direct.download(stop, new Runnable() {
-                @Override
-                public void run() {
-                    switch (info.getState()) {
-                    case DOWNLOADING:
-                        source.setState(States.DOWNLOADING);
-                        notify.run();
-                        break;
-                    case RETRYING:
-                        source.setState(States.RETRYING, info.getException());
-                        source.setDelay(info.getDelay());
-                        notify.run();
-                        break;
-                    default:
-                        // we can safely skip all statues. (extracting - already
-                        // pased, STOP / ERROR / DONE i will catch up here
+                if (infoOld.resume(infoNew)) {
+                    infoNew.copy(infoOld);
+                } else {
+                    if (target != null) {
+                        target.delete();
+                        target = null;
                     }
                 }
-            });
 
-            source.setState(States.DONE);
-            notify.run();
-        } catch (InterruptedException e) {
-            info.setState(URLInfo.States.STOPPED);
-            notify.run();
-            throw e;
-        } catch (RuntimeException e) {
-            info.setState(URLInfo.States.ERROR);
-            notify.run();
-            throw e;
+                retracted = true;
+            } catch (DownloadRetry ee) {
+                info.setState(States.RETRYING, ee);
+                notify.run();
+            }
         }
     }
 
+    void target(DownloadInfo dinfo) {
+        if (targetForce != null)
+            target = targetForce;
+
+        File f;
+
+        if (target == null) {
+            Integer idupcount = 0;
+
+            String sfilename = replaceBadChars(info.getTitle());
+
+            String ct = dinfo.getContentType();
+            if (ct == null)
+                throw new DownloadRetry("null content type");
+
+            String ext = ct.replaceFirst("video/", "").replaceAll("x-", "");
+
+            do {
+                String add = idupcount > 0 ? " (".concat(idupcount.toString()).concat(")") : "";
+
+                f = new File(targetDir, sfilename + add + "." + ext);
+                idupcount += 1;
+            } while (f.exists());
+
+            target = f;
+        }
+    }
+
+    public void download(final AtomicBoolean stop, final Runnable notify) {
+        try {
+            if (info.empty())
+                info.extract(stop, notify);
+
+            info.setState(States.EXTRACTING_DONE);
+            notify.run();
+
+            while (!done(stop)) {
+                try {
+                    final DownloadInfo dinfo = info.getInfo();
+
+                    target(dinfo);
+
+                    Direct direct;
+                    if (dinfo.range())
+                        direct = new DirectRange(dinfo, target);
+                    else
+                        direct = new DirectSingle(dinfo, target);
+
+                    if (dinfo.getContentType() == null || !dinfo.getContentType().contains("video/")) {
+                        throw new DownloadRetry("unable to download video, bad content");
+                    }
+
+                    direct.download(stop, new Runnable() {
+                        @Override
+                        public void run() {
+                            switch (dinfo.getState()) {
+                            case DOWNLOADING:
+                                info.setState(States.DOWNLOADING);
+                                notify.run();
+                                break;
+                            case RETRYING:
+                                info.setState(States.RETRYING, dinfo.getException());
+                                info.setDelay(dinfo.getDelay());
+                                notify.run();
+                                break;
+                            default:
+                                // we can safely skip all statues. (extracting -
+                                // already
+                                // pased, STOP / ERROR / DONE i will catch up
+                                // here
+                            }
+                        }
+                    });
+
+                    info.setState(States.DONE);
+                    notify.run();
+                } catch (DownloadRetry e) {
+                    retry(stop, notify, e);
+                } catch (DownloadIOError e) {
+                    retry(stop, notify, e);
+                }
+            }
+        } catch (DownloadInterruptedError e) {
+            info.setState(VideoInfo.States.STOP);
+            notify.run();
+
+            throw e;
+        } catch (RuntimeException e) {
+            info.setState(VideoInfo.States.ERROR);
+            notify.run();
+
+            throw e;
+        }
+    }
 }
